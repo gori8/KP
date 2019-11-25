91b7de62d7b5f9865bf88021ac2c7cf02bac36c0
gori8 <goriantolovic@gmail.com>
2019-11-25 22:05:56 +0100

    eureka zuul test

---
+++ test-api/src/main/java/com/example/testapi/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.testapi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    //@Autowired
+    //private DiscoveryClient discoveryClient;
+
+    @Value("${app.id}")
+    String  instance;
+
+    @RequestMapping("/")
+    public String helloFromTestApi(){
+        return "Hello from instance = " + instance;
+    }
+}
+
