95ac38a4d1de88fd94173b684070607cd79c72e7
gori8 <goriantolovic@gmail.com>
2019-11-25 03:37:51 +0100

    First commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,66 @@
+package com.example.demo;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.security.KeyStore;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@Bean
+	public RestTemplate restTemplate() throws Exception{
+		KeyStore clientStore = KeyStore.getInstance("JKS");
+		clientStore.load(new FileInputStream("src/main/resources/identity.jks"), "secret".toCharArray());
+		KeyStore trustStore = KeyStore.getInstance("JKS");
+		trustStore.load(new FileInputStream("src/main/resources/truststore.jks"), "secret".toCharArray());
+
+		SSLContextBuilder sslContextBuilder = new SSLContextBuilder();
+		sslContextBuilder.setProtocol("TLS");
+		sslContextBuilder.loadKeyMaterial(clientStore, "secret".toCharArray());
+		sslContextBuilder.loadTrustMaterial(trustStore,null);
+
+		SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContextBuilder.build());
+		CloseableHttpClient httpClient = HttpClients.custom()
+				.setSSLSocketFactory(sslConnectionSocketFactory)
+				.build();
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+		requestFactory.setConnectTimeout(10000); // 10 seconds
+		requestFactory.setReadTimeout(10000); // 10 seconds
+		return new RestTemplate(requestFactory);
+	}
+
+	@PostConstruct
+	private void init() throws Exception {
+		RestTemplate rt=restTemplate();
+		String fooResourceUrl
+				= "https://localhost:8443/api/hello";
+		ResponseEntity<String> response
+				= rt.getForEntity(fooResourceUrl,String.class);
+		System.out.println(response.getBody());
+	}
+
+}
