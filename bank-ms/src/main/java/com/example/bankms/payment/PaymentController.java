e44eb27faa88b44d8e68cf89bc5101ea7a4eecae
gori8 <goriantolovic@gmail.com>
2019-12-27 16:20:48 +0100

    Bug fixes

--- bank-ms/src/main/java/com/example/bankms/payment/PaymentController.java
+++ bank-ms/src/main/java/com/example/bankms/payment/PaymentController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @RestController
-@RequestMapping("/payment")
+@RequestMapping("/api/bankms")
 @CrossOrigin("*")
 public class PaymentController {
 
